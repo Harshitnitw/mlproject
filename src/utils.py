50c2c8d2e4cca9d78994076cd4e95aefac125906
Harshit Kedia <harshitkedia321@gmail.com>
2024-06-25 11:19:47 +0000

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import dill
 from sklearn.model_selection import GridSearchCV
 import numpy as np
 import pandas as pd
+import pickle
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -49,3 +50,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        # print('current working directory is: ',os.getcwd())
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
